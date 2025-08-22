47a2742b2ba50fd92623a14ab4e02924581ab499
DanaKhromenko <danadiadius@gmail.com>
2025-08-22 11:34:35 +0400

    Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.gmail.danadiadius"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
 
 repositories {
     mavenCentral()
